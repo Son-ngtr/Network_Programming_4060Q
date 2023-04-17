2e0756c6fc5cb27c0335b26d70dcc4eb71816f38
Son-ngtr <Son.Nguyen.20022020@gmail.com>
2023-04-17 23:50:17 +0700

    ..

--- Exercise/Assignment_1/4_Sv_Server.c
+++ Exercise/Assignment_1/4_Sv_Server.c
@@ -3,23 +3,25 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <unistd.h>
 #include <errno.h>
+#include <unistd.h>
+#include <time.h>
 
-#define MAX_CLIENT 5
-#define MAX_BUF_SIZE 1024
+#define MAX_LENGTH 1024
+#define MAX_CLIENT 10
 
 int main(int argc, char *argv[])
 {
+
     // Kiểm tra đầu vào
-    if (argc != 4)
+    if (argc != 3)
     {
-        printf("Usage: %s <Port> <The-file-contains-the-greeting> <The-file-that-stores-the-content-the-client-sends-to>\n", argv[0]);
+        printf("Usage: %s <port> <log-file>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     // Tạo socket
-    int server = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    int server = socket(AF_INET, SOCK_STREAM, 0);
     if (server == -1)
     {
         perror("socket() failed");
@@ -34,24 +36,25 @@ int main(int argc, char *argv[])
     server_addr.sin_port = htons(atoi(argv[1]));
 
     // Gán địa chỉ cho socket
-    if (bind(server, (struct sockaddr *)&server_addr,
-             sizeof(server_addr)) == -1)
+    if (bind(server, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
     {
         perror("bind() failed");
         exit(EXIT_FAILURE);
     }
 
-    // Lắng nghe kết nối từ client
+    // Lắng nghe kết nối
     if (listen(server, MAX_CLIENT) == -1)
     {
         perror("listen() failed");
         exit(EXIT_FAILURE);
     }
 
+    // Nhận dữ liệu từ client
+    char buf[MAX_LENGTH];
+    memset(buf, 0, MAX_LENGTH);
     while (1)
     {
-        printf("Waiting for client on %s %s\n",
-               inet_ntoa(server_addr.sin_addr), argv[1]);
+        printf("Waiting for client on %s %s\n", inet_ntoa(server_addr.sin_addr), argv[1]);
 
         // Chấp nhận kết nối từ client
         struct sockaddr_in client_addr;
@@ -70,41 +73,10 @@ int main(int argc, char *argv[])
                inet_ntoa(client_addr.sin_addr),
                ntohs(client_addr.sin_port));
 
-        // Đọc nội dung tệp tin chứa câu chào
-        FILE *fp = fopen(argv[2], "r");
-        if (fp == NULL)
-        {
-            perror("fopen() failed");
-            exit(EXIT_FAILURE);
-        }
-        fseek(fp, 0, SEEK_END);
-        long fsize = ftell(fp);
-        rewind(fp);
-        char *greeting = (char *)malloc(fsize + 1);
-        if (greeting == NULL)
-        {
-            perror("malloc() failed");
-            exit(EXIT_FAILURE);
-        }
-        memset(greeting, 0, fsize + 1);
-        fread(greeting, fsize, 1, fp);
-        fclose(fp);
-
-        // Gửi câu chào đến client
-        if (send(client, greeting, strlen(greeting), 0) == -1)
-        {
-            perror("send() failed");
-            exit(EXIT_FAILURE);
-        }
-        free(greeting);
-
-        // Nhận dữ liệu từ client
-        char buf[MAX_BUF_SIZE];
-        memset(buf, 0, MAX_BUF_SIZE);
         while (1)
         {
-            // Nhận dữ liệu từ client và ghi vào tệp tin
-            int bytes_received = recv(client, buf, MAX_BUF_SIZE, 0);
+            // Nhận dữ liệu từ client
+            int bytes_received = recv(client, buf, MAX_LENGTH, 0);
             buf[bytes_received] = '\0';
             if (bytes_received == -1)
             {
@@ -117,25 +89,25 @@ int main(int argc, char *argv[])
                 break;
             }
 
-            // In dữ liệu nhận được
-            printf("Received %ld bytes from client: %s", strlen(buf), buf);
+            // Lấy thời gian hiện tại
+            time_t current_time = time(NULL);
+            char *formatted_time = ctime(&current_time);
+            formatted_time[strlen(formatted_time) - 1] = '\0';
 
-            // Ghi dữ liệu vào tệp tin
-            fp = fopen(argv[3], "a");
-            if (fp == NULL)
+            // Ghi vào file log
+            FILE *log_file = fopen(argv[2], "a");
+            if (log_file == NULL)
             {
                 perror("fopen() failed");
                 exit(EXIT_FAILURE);
             }
-            fprintf(fp, "%s", buf);
-            fclose(fp);
+            printf("%s %s %s\n", inet_ntoa(client_addr.sin_addr), formatted_time, buf);
+            fprintf(log_file, "%s %s %s\n", inet_ntoa(client_addr.sin_addr), formatted_time, buf);
+            fclose(log_file);
         }
-        // Đóng kết nối với client
+
+        // Đóng kết nối
         close(client);
     }
-
-    // Đóng socket
-    close(server);
-
     return 0;
 }
